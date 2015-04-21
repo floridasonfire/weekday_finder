9198f49b2094b8651986c0ad9c19b2ed957587f0
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-04-21 10:00:57 -0700

    Added first spec

---
+++ spec/weekday_finder_spec.rb
@@ -0,0 +1,8 @@
+require ('rspec')
+require ('weekday_finder')
+
+describe('Time#weekday_finder') do
+it("will return the day of the week when user inputs date") do
+  expect((4/21/2015()).to(eq("Tuesday"))
+end
+end
