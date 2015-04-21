39d13bd9483c5102f482e5d6c984b096c826cfbd
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-04-21 10:14:33 -0700

    Add array spec

--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -2,7 +2,11 @@ require ('rspec')
 require ('weekday_finder')
 
 describe('Time#weekday_finder') do
+
+it("will change a date input in the form of 1/1/1111 to an array in the form of ["1", "1", "1111"]") do
+    expect((4/21/2015).weekday_finder()).to(eq(["4", "21", "2015" ]))
+
 it("will return the day of the week when user inputs date") do
-  expect((4/21/2015()).to(eq("Tuesday"))
+  expect((4/21/2015).weekday_finder()).to(eq("Tuesday"))
 end
 end
