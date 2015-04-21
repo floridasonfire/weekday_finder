81ee40b68ddd5eb14e63ccb78f47063f282d28c8
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-04-21 11:33:07 -0700

    Finish project

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -1,6 +1,22 @@
 class Array
   define_method(:weekday_finder) do
       user_time = Time.new(self.at(0), self.at(1), self.at(2))
-      user_time
+      if user_time.monday?()
+        "Monday"
+      elsif user_time.tuesday?()
+        "Tuesday"
+      elsif user_time.wednesday?()
+        "Wednesday"
+      elsif user_time.thursday?()
+        "Thursday"
+      elsif user_time.saturday?()
+        "Saturday"
+      elsif user_time.sunday?()
+        "Sunday"
+      else
+        "Friday"
+      end
   end
+
+
 end
--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -3,9 +3,9 @@ require ('weekday_finder.rb')
 
 describe('Array#weekday_finder') do
 
-it("will turn a user's date input into a Time.new() object") do
-  expect([2015, 3, 6].weekday_finder()).to(eq(Time.new(2015, 3, 6)))
-end
+#it("will turn a user's date input into a Time.new() object") do
+#  expect([2015, 3, 6].weekday_finder()).to(eq(Time.new(2015, 3, 6)))
+# end
 
 it("will return the day of the week when user inputs date") do
   expect([2015, 4, 21].weekday_finder()).to(eq("Tuesday"))
