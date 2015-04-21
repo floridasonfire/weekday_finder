9e6fe4973b7ef46f2908af8f8808ee5d15c9fa19
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-04-21 11:22:47 -0700

    Switch class to Array, try again

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -1,10 +1,7 @@
-class String
-  user_date_array = []
+class Time
+
   define_method(:weekday_finder) do
-    user_date_array = self.split("/")
-    user_date_array.each do |number|
-      number.to_i
-    end
-    user_date_array
+      user_time = Time.new (self)
+      puts user_time
   end
 end
--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -1,20 +1,13 @@
 require ('rspec')
 require ('weekday_finder.rb')
 
-describe('String#weekday_finder') do
+describe('Array#weekday_finder') do
 
-it("will change a date input in the form of 1/1/1111 to an array in the form of ['1', '1', '1111']") do
-  expect(("4/21/2015").weekday_finder()).to(eq(["4", "21", "2015" ]))
-end
-it("will take that array and turn its elements into integers") do
-  expect((3/6/2014).weekday_finder()).to(eq([3, 6, 2014]))
-end
-
-it("will pull out integers from array into format compatible with Time.new()") do
-  expect((3/6/2013).weekday_finder()).to(eq(Time.new(2015, 3, 6)))
+it("will turn a user's date input into a Time.new() object") do
+  expect([2015, 3, 6].weekday_finder()).to(eq(Time.new(2015, 3, 6)))
 end
 
 it("will return the day of the week when user inputs date") do
-  expect((4/21/2015).weekday_finder()).to(eq("Tuesday"))
+  expect([2015, 4, 21].weekday_finder()).to(eq("Tuesday"))
 end
 end
