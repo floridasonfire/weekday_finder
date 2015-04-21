490d191518166899097f9d89f8ba07881f25c433
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-04-21 11:03:02 -0700

    Change class to time, starting over.

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -3,8 +3,8 @@ class String
   define_method(:weekday_finder) do
     user_date_array = self.split("/")
     user_date_array.each do |number|
-      number.to_i!
+      number.to_i
     end
-    
+    user_date_array
   end
 end
